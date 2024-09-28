cf424ed8e4d38e58c82c1066691e091f89906581
DarkInventor <ktmehta25@gmail.com>
2024-09-28 15:19:09 -0400

    make flip toggle responsive

--- components/easy-hero.tsx
+++ components/easy-hero.tsx
@@ -99,39 +99,16 @@ const EasyHero: React.FC<EasyHeroProps> = ({ title, subtext, heroimage }) => {
             {subtext}
           </p>
         </main>
-        <button
-          onClick={toggleTheme}
-          className="fixed top-[152px] lg:top-40 right-20 lg:right-20 mr-20 lg:mr-20 p-2 rounded-md  dark:bg-black hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors "
-        >
-          <div>
+        <div className="absolute top-5 right-5 lg:right-20 p-2 rounded-md dark:bg-black hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors z-50">
+          <button onClick={toggleTheme} className="flex items-center justify-center">
             {theme === "dark" ? (
-              // <svg
-              //   fill="currentColor"
-              //   xmlns="http://www.w3.org/2000/svg"
-              //   viewBox="0 0 24 24"
-              //   className="w-6 h-6"
-              // >
-              //   <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
-              // </svg>
-              <FlipHorizontalIcon className="h-6 w-6 ml-20 fixed" />
+              <FlipHorizontalIcon className="h-6 w-6" />
             ) : (
-              // <svg
-              //   fill="currentColor"
-              //   xmlns="http://www.w3.org/2000/svg"
-              //   viewBox="0 0 24 24"
-              //   className="w-6 h-6"
-              // >
-              //   <path
-              //     fillRule="evenodd"
-              //     d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
-              //     clipRule="evenodd"
-              //   />
-              // </svg>
-              <FlipHorizontal2 className="h-6 w-6 ml-20 fixed" />
+              <FlipHorizontal2 className="h-6 w-6" />
             )}
-          </div>
-          <span className="sr-only">Toggle Theme</span>
-        </button>
+            <span className="sr-only">Toggle Theme</span>
+          </button>
+        </div>
       </motion.div>
     </>
   )
